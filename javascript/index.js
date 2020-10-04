c380a595a0f4c4d06fcff8eaf9ffd1c8ec0675de
Francisco <cisco.fv.29gmail.com>
2020-10-03 20:08:48 -0500

    cambio de carrusel

--- javascript/index.js
+++ javascript/index.js
@@ -1,49 +1,3 @@
-let imagenes = [
-	"imagenes/imag1.jpg",
-	"imagenes/imag2.jpg",
-	"imagenes/imag3.jpg",
-	"imagenes/imag4.jpg",
-];
-
-let imagen = document.getElementById("img1");
-let preIm = document.getElementById("img2");
-let postIm = document.getElementById("img3");
-let order = 0;
-
-function siguiente() {
-	if (order === 0) {
-		order = imagenes.length - 1;
-	} else {
-		order -= 1;
-	}
-	postIm.src = imagenes[order];
-	imagen.className = "img1";
-	postIm.className = "img1";
-	function cambio() {
-		imagen.src = postIm.src;
-		imagen.className = "";
-		postIm.className = "";
-	}
-	setTimeout(cambio, 850);
-}
-
-function anterior() {
-	if (order + 2 > imagenes.length) {
-		order = 0;
-	} else {
-		order += 1;
-	}
-	preIm.src = imagenes[order];
-	imagen.className = "img3";
-	preIm.className = "img3";
-	function seguir() {
-		imagen.src = preIm.src;
-		imagen.className = "";
-		preIm.className = "";
-	}
-	setTimeout(seguir, 850);
-}
-
 addEventListener("scroll", () => {
 	// animaciones de los servicios en la parte inferios del index
 	let services = document.getElementsByClassName("servicio-ind");
@@ -60,23 +14,35 @@ addEventListener("scroll", () => {
 			}
 		}
 	}
-	// Cambiar la navegación de estatcia a fixed
-	// let distanciaHeader = document.getElementById("navegacion-static");
+});
 
-	// if (distanciaHeader != null) {
-	// 	let distancia = Math.round(distanciaHeader.getBoundingClientRect().bottom);
-	// 	let menuInterno = document.getElementById("menu-static");
-	// 	if (distancia <= 5) {
-	// 		distanciaHeader.id = "navegacion";
-	// 		menuInterno.id = "menu";
-	// 	}
-	// } else {
-	// 	let distanciaHeader = document.getElementById("navegacion");
-	// 	let distancia = Math.round(distanciaHeader.getBoundingClientRect().bottom);
-	// 	let menuInterno = document.getElementById("menu");
-	// 	if (distancia >= 6) {
-	// 		distanciaHeader.id = "navegacion-static";
-	// 		menuInterno.id = "menu-static";
-	// 	}
-	// }
+window.addEventListener("load", function () {
+	new Glider(document.querySelector(".carousel__lista"), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: ".carousel__indicadores",
+		arrows: {
+			prev: ".carousel__anterior",
+			next: ".carousel__siguiente",
+		},
+		responsive: [
+			{
+				// screens greater than >= 775px
+				breakpoint: 450,
+				settings: {
+					// Set to `auto` and provide item width to adjust to viewport
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				// screens greater than >= 1024px
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				},
+			},
+		],
+	});
 });
