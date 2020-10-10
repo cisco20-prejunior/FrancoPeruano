94ae06b41a9981980c1672d7e6a210333dac638c
Francisco <cisco.fv.29gmail.com>
2020-10-09 20:25:55 -0500

    Agregado el popUp

--- javascript/index.js
+++ javascript/index.js
@@ -55,3 +55,19 @@ window.addEventListener("load", function () {
 		],
 	});
 });
+
+let btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+window.addEventListener('load', () =>{
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
